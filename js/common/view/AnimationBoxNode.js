57d3f7c27db58f03e646c4abf76528539c451a9f
samreid <reids@colorado.edu>
2015-11-06 20:50:41 -0700

    updated copyright info using grunt update-copyright-dates, see  https://github.com/phetsims/chipper/issues/403

--- js/common/view/AnimationBoxNode.js
+++ js/common/view/AnimationBoxNode.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2015, University of Colorado Boulder
+// Copyright 2015, University of Colorado Boulder
 
 /**
  * AnimationBoxNode.js - Node containing animation area for alpha particles.
--- js/plum-pudding-atom/PlumPuddingAtomScreen.js
+++ js/plum-pudding-atom/PlumPuddingAtomScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2015, University of Colorado Boulder
+// Copyright 2015, University of Colorado Boulder
 
 /**
  * The 'Plum Pudding Atom' screen. Conforms to the contract specified in joist/Screen.
