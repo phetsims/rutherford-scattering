e9a8bcab87d323cf04e6d5c392f24e4ee9d5a4e5
Dave 'Smitty' Schmitz <smitty@SchmitzwarePro.local>
2016-03-09 15:18:23 -0800

    Plum pudding new image adjustments.

--- js/plumpuddingatom/view/PlumPuddingSpaceNode.js
+++ js/plumpuddingatom/view/PlumPuddingSpaceNode.js
@@ -14,9 +14,6 @@ define( function( require ) {
   var ParticleSpaceNode = require( 'RUTHERFORD_SCATTERING/common/view/ParticleSpaceNode' );
   var PlumPuddingAtomNode = require( 'RUTHERFORD_SCATTERING/plumpuddingatom/view/PlumPuddingAtomNode' );
 
-  // constants
-  var ATOM_IMAGE_MARGIN = 75;
-
   /**
    * @param {RSBaseModel} model
    * @param {Property.<boolean>} showAlphaTraceProperty
@@ -33,7 +30,7 @@ define( function( require ) {
     // plum pudding image - calc image scale and center positioning
     this.atomNode = new PlumPuddingAtomNode();
     var scale = Math.min( this.width, this.height ) /
-                ( Math.max( this.atomNode.width, this.atomNode.height ) + 2 * ATOM_IMAGE_MARGIN );
+                ( Math.max( this.atomNode.width, this.atomNode.height ) );
     var imageWidth = this.atomNode.width * scale;
     var imageHeight = this.atomNode.height * scale;
     var imageX = this.bounds.centerX - imageWidth / 2;
