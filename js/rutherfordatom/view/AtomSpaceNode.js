d1e550c0ebeae5e1585dc4e55f4867daf9135d59
phet-dev <phetadmin@gmail.com>
2019-07-31 03:38:27 -0600

    update copyright dates from daily grunt work

--- js/rutherfordatom/view/AtomSpaceNode.js
+++ js/rutherfordatom/view/AtomSpaceNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, University of Colorado Boulder
+// Copyright 2016-2019, University of Colorado Boulder
 
 /**
  * Space in which atoms and alpha particles are rendered.  In this representation, there are several
