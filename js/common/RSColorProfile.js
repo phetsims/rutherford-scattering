29719f1fe92baebe354c52b907c736c76108cbe2
phet-dev <phetadmin@gmail.com>
2020-02-28 03:47:49 -0700

    update copyright dates from daily grunt work

--- js/common/RSColorProfile.js
+++ js/common/RSColorProfile.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, University of Colorado Boulder
+// Copyright 2016-2020, University of Colorado Boulder
 
 /**
  * Location for all colors, especially those that must change for projector mode.
