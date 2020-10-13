94912bc8eb1e4faa93fdd04dbae8fa516eda6f47
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 16:24:39 -0600

    convert ParticleAtom subclass to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/rutherfordatom/model/RutherfordNucleus.js
+++ js/rutherfordatom/model/RutherfordNucleus.js
@@ -8,63 +8,63 @@
  */
 
 import Utils from '../../../../dot/js/Utils.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import Particle from '../../../../shred/js/model/Particle.js';
 import ParticleAtom from '../../../../shred/js/model/ParticleAtom.js';
 import rutherfordScattering from '../../rutherfordScattering.js';
 
-/**
- * @param {Property.<number>} protonCountProperty
- * @param {Property.<number>} neutronCountProperty
- * @constructor
- */
-function RutherfordNucleus( protonCountProperty, neutronCountProperty ) {
+class RutherfordNucleus extends ParticleAtom {
 
-  ParticleAtom.call( this, {
-    nucleonRadius: 3
-  } );
-  const self = this;
-  let particle;
+  /**
+   * @param {Property.<number>} protonCountProperty
+   * @param {Property.<number>} neutronCountProperty
+   */
+  constructor( protonCountProperty, neutronCountProperty ) {
 
-  // update number of nucleons of a particular type and move all to destination
-  const configureNucleus = function( nucleonCount, particleType ) {
-    const particleCount = Utils.toFixedNumber( nucleonCount, 0 );
-    const nucleons = particleType === 'proton' ? self.protons : self.neutrons;
-    while ( nucleons.length !== particleCount ) {
-      if ( particleCount - nucleons.length > 0 ) {
-        particle = new Particle( particleType );
-        self.addParticle( particle );
+    super( {
+      nucleonRadius: 3
+    } );
+
+    const self = this;
+    let particle;
+
+    // update number of nucleons of a particular type and move all to destination
+    const configureNucleus = function( nucleonCount, particleType ) {
+      const particleCount = Utils.toFixedNumber( nucleonCount, 0 );
+      const nucleons = particleType === 'proton' ? self.protons : self.neutrons;
+      while ( nucleons.length !== particleCount ) {
+        if ( particleCount - nucleons.length > 0 ) {
+          particle = new Particle( particleType );
+          self.addParticle( particle );
+        }
+        else {
+          self.extractParticle( particleType );
+          self.reconfigureNucleus();
+        }
       }
-      else {
-        self.extractParticle( particleType );
-        self.reconfigureNucleus();
-      }
-    }
-    self.moveAllParticlesToDestination();
-  };
+      self.moveAllParticlesToDestination();
+    };
 
-  const protonObserver = function( protonCount ) { configureNucleus( protonCount, 'proton' ); };
-  const neutronObserver = function( neutronCount ) { configureNucleus( neutronCount, 'neutron' ); };
-  protonCountProperty.link( protonObserver );
-  neutronCountProperty.link( neutronObserver );
+    const protonObserver = function( protonCount ) { configureNucleus( protonCount, 'proton' ); };
+    const neutronObserver = function( neutronCount ) { configureNucleus( neutronCount, 'neutron' ); };
+    protonCountProperty.link( protonObserver );
+    neutronCountProperty.link( neutronObserver );
 
-  // @private
-  this.disposeRutherfordNucleus = function() {
-    protonCountProperty.unlink( protonObserver );
-    neutronCountProperty.unlink( neutronObserver );
-  };
+    // @private
+    this.disposeRutherfordNucleus = function() {
+      protonCountProperty.unlink( protonObserver );
+      neutronCountProperty.unlink( neutronObserver );
+    };
+  }
+
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    this.disposeRutherfordNucleus();
+    super.dispose();
+  }
 }
 
 rutherfordScattering.register( 'RutherfordNucleus', RutherfordNucleus );
-
-inherit( ParticleAtom, RutherfordNucleus, {
-
-  /**
-   * Make nucleus eligible for garbage collection.
-   */
-  dispose: function() {
-    this.disposeRutherfordNucleus();
-  }
-} );
-
 export default RutherfordNucleus;
