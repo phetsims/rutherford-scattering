7ea7eef90ea2b19d43029952016c67f7c089e6a9
samreid <reids@colorado.edu>
2021-07-18 13:25:51 -0600

    Use ProfileColorProperty, see https://github.com/phetsims/scenery-phet/issues/515

--- js/common/RSColorProfile.js
+++ js/common/RSColorProfile.js
@@ -7,81 +7,81 @@
  * @author Jesse Greenberg
  */
 
-import ColorProfile from '../../../scenery-phet/js/ColorProfile.js';
 import FocusHighlightPath from '../../../scenery/js/accessibility/FocusHighlightPath.js';
 import Color from '../../../scenery/js/util/Color.js';
+import ProfileColorProperty from '../../../scenery/js/util/ProfileColorProperty.js';
 import rutherfordScattering from '../rutherfordScattering.js';
 
 // Initial colors for each profile, by string key.  If a projector color is not defined, it will take
 // the 'default' value provided.
 // NOTE: This is NOT provided to clients directly, but each of these are passed to the Property constructor,
 // see ColorProfile.js
-const RSColorProfile = new ColorProfile( [ 'default', 'projector' ], {
-  backgroundColor: {
+const RSColorProfile = {
+  backgroundColorProperty: new ProfileColorProperty( 'backgroundColor', {
     default: new Color( 0, 0, 0 ),
     projector: new Color( 255, 255, 255 )
-  },
-  panelColor: {
+  } ),
+  panelColorProperty: new ProfileColorProperty( 'panelColor', {
     default: new Color( 0, 0, 0 ),
     projector: new Color( 255, 255, 255 )
-  },
-  radioButtonBorderColor: {
+  } ),
+  radioButtonBorderColorProperty: new ProfileColorProperty( 'radioButtonBorderColor', {
     default: new Color( 200, 200, 95 ),
     projector: new Color( 49, 193, 221 )
-  },
-  panelBorderColor: {
+  } ),
+  panelBorderColorProperty: new ProfileColorProperty( 'panelBorderColor', {
     default: new Color( 128, 128, 128 ),
     projector: new Color( 128, 128, 128 )
-  },
-  panelTitleColor: {
+  } ),
+  panelTitleColorProperty: new ProfileColorProperty( 'panelTitleColor', {
     default: new Color( 200, 200, 95 ),
     projector: new Color( 0, 0, 0 )
-  },
-  panelLabelColor: {
+  } ),
+  panelLabelColorProperty: new ProfileColorProperty( 'panelLabelColor', {
     default: new Color( 210, 210, 210 ),
     projector: new Color( 0, 0, 0 )
-  },
-  protonsLabelColor: {
+  } ),
+  protonsLabelColorProperty: new ProfileColorProperty( 'protonsLabelColor', {
     default: new Color( 185, 50, 8 ),
     projector: new Color( 185, 50, 8 )
-  },
-  neutronsLabelColor: {
+  } ),
+  neutronsLabelColorProperty: new ProfileColorProperty( 'neutronsLabelColor', {
     default: new Color( 160, 160, 160 ),
     projector: new Color( 125, 125, 125 )
-  },
-  atomBeamColor: {
+  } ),
+  atomBeamColorProperty: new ProfileColorProperty( 'atomBeamColor', {
     default: new Color( 143, 143, 143, 0.4 ),
     projector: new Color( 143, 143, 143, 0.4 )
-  },
-  nucleusBeamColor: {
+  } ),
+  nucleusBeamColorProperty: new ProfileColorProperty( 'nucleusBeamColor', {
     default: new Color( 143, 143, 143 ),
     projector: new Color( 163, 163, 163 )
-  },
-  nucleusColor: {
+  } ),
+  nucleusColorProperty: new ProfileColorProperty( 'nucleusColor', {
     default: new Color( 200, 200, 95 ),
     projector: new Color( 128, 128, 0 )
-  },
-  panelSliderLabelColor: {
+  } ),
+  panelSliderLabelColorProperty: new ProfileColorProperty( 'panelSliderLabelColor', {
     default: new Color( 180, 180, 180 ),
     projector: new Color( 0, 0, 0 )
-  },
-  screenIconFillColor: {
+  } ),
+  screenIconFillColorProperty: new ProfileColorProperty( 'screenIconFillColor', {
     default: new Color( 0, 0, 0 ),
     projector: new Color( 0, 0, 0 )
-  },
-  nucleusOutlineColor: {
+  } ),
+  nucleusOutlineColorProperty: new ProfileColorProperty( 'nucleusOutlineColor', {
     default: new Color( 255, 255, 255 ),
     projector: new Color( 0, 0, 0 )
-  },
-  innerGroupHighlightColor: {
+  } ),
+  innerGroupHighlightColorProperty: new ProfileColorProperty( 'innerGroupHighlightColor', {
     default: FocusHighlightPath.INNER_DARK_GROUP_FOCUS_COLOR,
     projector: FocusHighlightPath.INNER_LIGHT_GROUP_FOCUS_COLOR
-  },
-  outerGroupHighlightColor: {
+  } ),
+  outerGroupHighlightColorProperty: new ProfileColorProperty( 'outerGroupHighlightColor', {
     default: FocusHighlightPath.OUTER_DARK_GROUP_FOCUS_COLOR,
     projector: FocusHighlightPath.OUTER_LIGHT_GROUP_FOCUS_COLOR
-  }
-} );
+  } )
+};
 
 rutherfordScattering.register( 'RSColorProfile', RSColorProfile );
 
