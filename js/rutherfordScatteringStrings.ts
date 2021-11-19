0fed3bac5d56c83b9f6f68a313a0482e4ee927d7
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:31 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/rutherfordScatteringStrings.ts
@@ -0,0 +1,70 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import rutherfordScattering from './rutherfordScattering.js';
+
+type StringsType = {
+  'rutherford-scattering': {
+    'title': string
+  },
+  'rutherfordAtom': string,
+  'plumPuddingAtom': string,
+  'alphaParticleProperties': string,
+  'energy': string,
+  'atom': string,
+  'numberOfProtons': string,
+  'numberOfNeutrons': string,
+  'minEnergy': string,
+  'maxEnergy': string,
+  'showTraces': string,
+  'alphaParticle': string,
+  'alphaParticles': string,
+  'positiveCharge': string,
+  'proton': string,
+  'electron': string,
+  'neutron': string,
+  'nucleus': string,
+  'electronEnergyLevel': string,
+  'alphaParticleTrace': string,
+  'legend': string,
+  'pattern': {
+    '0atomicScale': string,
+    '0nuclearScale': string
+  },
+  'a11y': {
+    'screenSummary': string,
+    'observationWindow': string,
+    'atomSpaceDescription': string,
+    'nucleusSpaceDescription': string,
+    'toggleAlphaParticle': string,
+    'alphaParticlesHelpText': string,
+    'alphaParticleSettings': string,
+    'energySliderDescription': string,
+    'energy': string,
+    'traces': string,
+    'traceCheckboxDescription': string,
+    'atomSettings': string,
+    'protonsValuePattern': string,
+    'protonsPerAtomValuePattern': string,
+    'protonSliderDescription': string,
+    'neutronsValuePattern': string,
+    'neutronsPerAtomValuePattern': string,
+    'neutronSliderDescription': string,
+    'atomicScaleView': string,
+    'nuclearScaleView': string,
+    'switchScale': string,
+    'switchScaleDescription': string,
+    'otherViewingStreamingOptions': string,
+    'otherOptionsDescription': string
+  }
+};
+
+const rutherfordScatteringStrings = getStringModule( 'RUTHERFORD_SCATTERING' ) as StringsType;
+
+rutherfordScattering.register( 'rutherfordScatteringStrings', rutherfordScatteringStrings );
+
+export default rutherfordScatteringStrings;
