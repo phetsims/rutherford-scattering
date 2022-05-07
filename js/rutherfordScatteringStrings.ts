95582cfa2de0b9b1bc4a586af970a1f5b73e793f
Sam Reid <reids@colorado.edu>
2022-05-07 09:33:57 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/rutherfordScatteringStrings.ts
+++ js/rutherfordScatteringStrings.ts
@@ -9,57 +9,57 @@ import rutherfordScattering from './rutherfordScattering.js';
 
 type StringsType = {
   'rutherford-scattering': {
-    'title': string
-  },
-  'rutherfordAtom': string,
-  'plumPuddingAtom': string,
-  'alphaParticleProperties': string,
-  'energy': string,
-  'atom': string,
-  'numberOfProtons': string,
-  'numberOfNeutrons': string,
-  'minEnergy': string,
-  'maxEnergy': string,
-  'showTraces': string,
-  'alphaParticle': string,
-  'alphaParticles': string,
-  'positiveCharge': string,
-  'proton': string,
-  'electron': string,
-  'neutron': string,
-  'nucleus': string,
-  'electronEnergyLevel': string,
-  'alphaParticleTrace': string,
-  'legend': string,
+    'title': string;
+  };
+  'rutherfordAtom': string;
+  'plumPuddingAtom': string;
+  'alphaParticleProperties': string;
+  'energy': string;
+  'atom': string;
+  'numberOfProtons': string;
+  'numberOfNeutrons': string;
+  'minEnergy': string;
+  'maxEnergy': string;
+  'showTraces': string;
+  'alphaParticle': string;
+  'alphaParticles': string;
+  'positiveCharge': string;
+  'proton': string;
+  'electron': string;
+  'neutron': string;
+  'nucleus': string;
+  'electronEnergyLevel': string;
+  'alphaParticleTrace': string;
+  'legend': string;
   'pattern': {
-    '0atomicScale': string,
-    '0nuclearScale': string
-  },
+    '0atomicScale': string;
+    '0nuclearScale': string;
+  };
   'a11y': {
-    'screenSummary': string,
-    'observationWindow': string,
-    'atomSpaceDescription': string,
-    'nucleusSpaceDescription': string,
-    'toggleAlphaParticle': string,
-    'alphaParticlesHelpText': string,
-    'alphaParticleSettings': string,
-    'energySliderDescription': string,
-    'energy': string,
-    'traces': string,
-    'traceCheckboxDescription': string,
-    'atomSettings': string,
-    'protonsValuePattern': string,
-    'protonsPerAtomValuePattern': string,
-    'protonSliderDescription': string,
-    'neutronsValuePattern': string,
-    'neutronsPerAtomValuePattern': string,
-    'neutronSliderDescription': string,
-    'atomicScaleView': string,
-    'nuclearScaleView': string,
-    'switchScale': string,
-    'switchScaleDescription': string,
-    'otherViewingStreamingOptions': string,
-    'otherOptionsDescription': string
+    'screenSummary': string;
+    'observationWindow': string;
+    'atomSpaceDescription': string;
+    'nucleusSpaceDescription': string;
+    'toggleAlphaParticle': string;
+    'alphaParticlesHelpText': string;
+    'alphaParticleSettings': string;
+    'energySliderDescription': string;
+    'energy': string;
+    'traces': string;
+    'traceCheckboxDescription': string;
+    'atomSettings': string;
+    'protonsValuePattern': string;
+    'protonsPerAtomValuePattern': string;
+    'protonSliderDescription': string;
+    'neutronsValuePattern': string;
+    'neutronsPerAtomValuePattern': string;
+    'neutronSliderDescription': string;
+    'atomicScaleView': string;
+    'nuclearScaleView': string;
+    'switchScale': string;
+    'switchScaleDescription': string;
+    'otherViewingStreamingOptions': string;
+    'otherOptionsDescription': string;
   }
 };
 
