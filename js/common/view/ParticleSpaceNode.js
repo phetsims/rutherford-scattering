4d415ad5ae7b1dc5d5c6c13f788cd9d31010e0f9
phet-dev <phetadmin@gmail.com>
2022-07-21 05:37:16 -0600

    update copyright dates from daily grunt work

--- js/common/view/ParticleSpaceNode.js
+++ js/common/view/ParticleSpaceNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * The space in which atoms and alpha particles are rendered.  The particles can be represented two
--- js/common/view/RSControlPanel.js
+++ js/common/view/RSControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 
 /**
  * Collection of all control panels in a screen of this sim.  Different scenes in a single
