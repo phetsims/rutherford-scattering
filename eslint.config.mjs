a4d497a7fbe71222a96680e659af59e49d881ed9
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:32 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for rutherford-scattering.
+ * ESLint configuration for rutherford-scattering.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
