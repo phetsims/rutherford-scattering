9a01fd50d72fa61daaee77141e10f989f69a721a
phet-dev <phetadmin@gmail.com>
2024-10-28 04:01:03 -0600

    update copyright dates from daily grunt work

--- js/common/view/RSScreenSummaryNode.js
+++ js/common/view/RSScreenSummaryNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2024, University of Colorado Boulder
 
 /**
  * A consistent Scene Summary node that can be used in all screen views.
