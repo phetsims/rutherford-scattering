8aa9dda8174e0b8f4b3dcd28295c3292eb60aeed
phet-dev <phetadmin@gmail.com>
2026-01-28 04:14:17 -0700

    update copyright dates from daily grunt work

--- js/common/view/NuclearParticleLegendPanel.ts
+++ js/common/view/NuclearParticleLegendPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2025, University of Colorado Boulder
+// Copyright 2016-2026, University of Colorado Boulder
 
 /**
  * Legend for particles related to the nuclear views in the sim
--- js/rutherfordatom/view/AtomParticleLegendPanel.ts
+++ js/rutherfordatom/view/AtomParticleLegendPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2025, University of Colorado Boulder
+// Copyright 2016-2026, University of Colorado Boulder
 
 /**
  * Legend for particles related to the atom screens and scenes in the sim.
