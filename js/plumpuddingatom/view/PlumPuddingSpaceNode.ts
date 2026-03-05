b193c001edfa1b56e80c6596c65a06e9fd611416
phet-dev <phetadmin@gmail.com>
2026-03-05 05:57:50 -0700

    update copyright dates from daily grunt work

--- js/plumpuddingatom/view/PlumPuddingSpaceNode.ts
+++ js/plumpuddingatom/view/PlumPuddingSpaceNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2025, University of Colorado Boulder
+// Copyright 2016-2026, University of Colorado Boulder
 
 /**
  * PlumPuddingSpaceNode is the space in which atoms and alpha particles are rendered.
