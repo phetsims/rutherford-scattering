ab1bb7f257bb9103e2d7673ebd56570617f91713
Marla Schulz <marla.schulz@colorado.edu>
2026-06-15 11:56:31 -0600

    sync-to-poly: Move Marla to team, see: https://github.com/phetsims/rutherford-scattering/issues/199
    Mono-SHA: 959c273f430c4eaf247eba81ac448889bd2cf839

--- js/rutherford-scattering-main.ts
+++ js/rutherford-scattering-main.ts
@@ -19,9 +19,9 @@ const rutherfordScatteringTitleStringProperty = RutherfordScatteringFluent[ 'rut
 const simOptions: SimOptions = {
   credits: {
     leadDesign: 'Amy Hanson, Sam McKagan',
-    softwareDevelopment: 'Jesse Greenberg, Chris Malley, Dave Schmitz, Marla Schulz, Agust\u00EDn Vallejo',
+    softwareDevelopment: 'Jesse Greenberg, Chris Malley, Dave Schmitz, Agust\u00EDn Vallejo',
     team: 'Wendy Adams, Michael Dubson, Noah Finkelstein, Danielle Harlow, Emily B. Moore, Ariel Paul, ' +
-          'Kathy Perkins, Noah Podolefsky, Amy Rouinfar, Taliesin Smith, Carl Wieman',
+          'Kathy Perkins, Noah Podolefsky, Amy Rouinfar, Marla Schulz, Taliesin Smith, Carl Wieman',
     qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Andrea Lin, Matthew Moore, ' +
                       'Valentina P\u00E9rez, Ben Roberts, Nancy Salpepi, Kathryn Woessner, Bryan Yoelin',
     graphicArts: '',
